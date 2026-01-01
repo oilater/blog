f53f4c268e4bdbf5ccff1d1873256cc5cc260067
oilater <oilater@naver.com>
2026-01-01 16:35:10 +0900

    🔧 Remove Button component

--- apps/portfolio/src/components/styles/Article.css.ts
+++ apps/portfolio/src/components/styles/Article.css.ts
@@ -80,15 +80,3 @@ export const articleContent = style({
     },
   },
 });
-
-export const button = style({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: '#C3C3C6',
-  fontSize: '1.2rem',
-  fontWeight: 500,
-  background: 'none',
-  border: 'none',
-  cursor: 'pointer',
-});
