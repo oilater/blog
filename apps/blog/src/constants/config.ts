9dc781e3b88496c563273c0b3cd35ff51d3e2385
oilater <oilater@naver.com>
2026-01-19 18:59:05 +0900

    🔧 Rename blog

--- apps/blog/src/constants/config.ts
+++ apps/blog/src/constants/config.ts
@@ -1,13 +1,12 @@
 export const BlogConfig = {
   url: 'https://oilater.com',
-  title: '프론트엔드 개발자 김성현 포트폴리오',
-  description: '프론트엔드 개발자 김성현 포트폴리오',
+  title: 'SeongHyeon Kim',
+  description: '프론트엔드 개발자 김성현 블로그',
   keywords: [
-    '프론트엔드 김성현 포트폴리오',
-    '프론트엔드 포트폴리오',
+    '프론트엔드 김성현 블로그',
+    '프론트엔드 개발자 김성현',
     '프론트엔드 개발자 포트폴리오',
     '개발자 포트폴리오',
-    '신입 개발자 포트폴리오',
   ],
   velogId: 'oilater',
   menu: [
