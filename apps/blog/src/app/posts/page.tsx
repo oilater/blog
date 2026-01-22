48bc6ad4b49199684b31fe054819c58b7cf7477c
oilater <oilater@naver.com>
2026-01-22 19:33:16 +0900

    🔥 Remove searchParams

--- apps/blog/src/app/posts/page.tsx
+++ apps/blog/src/app/posts/page.tsx
@@ -1,20 +1,15 @@
-import { getAllTags, getPostsByTag } from '#/lib/posts';
+import { getAllPosts, getAllTags } from '#/lib/posts';
 import { PostsList } from './components/PostsList';
 import { TagFilter } from './components/TagFilter';
 
-interface SearchParams {
-  tag?: string;
-}
-
-export default async function PostsPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
-  const { tag } = await searchParams;
+export default function PostsPage() {
   const allTags = getAllTags();
-  const filteredPosts = getPostsByTag(tag);
+  const allPosts = getAllPosts();
 
   return (
     <>
-      <TagFilter tags={allTags} selectedTag={tag} />
-      <PostsList posts={filteredPosts} />
+      <TagFilter tags={allTags} />
+      <PostsList posts={allPosts} />
     </>
   );
 }
