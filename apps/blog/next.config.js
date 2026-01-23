7c6d9b1587b0c0111d4fda300382e324e60c51ff
oilater <oilater@naver.com>
2026-01-23 23:22:43 +0900

    🔧 Modify deviceSizes

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   turbopack: {},
   images: {
     formats: ['image/avif', 'image/webp'],
-    deviceSizes: [480, 640, 680, 750, 828, 1080, 1200],
+    deviceSizes: [680, 750, 1080],
     qualities: [75, 80],
   },
 };
