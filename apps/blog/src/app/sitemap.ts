04f5c202812d1e865e311d3218069289c70108c5
oilater <oilater@naver.com>
2026-01-24 09:43:35 +0900

    🔧 Update sitemap

--- apps/blog/src/app/sitemap.ts
+++ apps/blog/src/app/sitemap.ts
@@ -3,9 +3,5 @@ import type { MetadataRoute } from 'next';
 const BASE_URL = 'https://oilater.com';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return [
-    { url: BASE_URL, lastModified: new Date() },
-    { url: `${BASE_URL}/feed`, lastModified: new Date() },
-    { url: `${BASE_URL}/about`, lastModified: new Date() },
-  ];
+  return [{ url: BASE_URL, lastModified: new Date() }];
 }
